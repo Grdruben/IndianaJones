21ee9f1c7446add0583f39f57e9b77d90785bd90
Ruben GAGNON <delphingagnon2@gmail.com>
2022-04-01 11:58:19 +0200

    Ajout d

---
+++ src/models/Itinerary.js
@@ -0,0 +1,13 @@
+const constants = require('../utils/constants');
+const moment = require('moment');
+
+
+class Itinerary {
+   constructor() {
+           this.data = {
+               itineraryList: new Array()
+           };
+   }
+}
+
+module.exports.Itinerary = Itinerary;
